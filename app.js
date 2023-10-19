893fba5a05852a648a5180c66aef8ce298bd5483
Mackenzzy <mackenzzy@gmail.com>
2023-10-18 21:44:20 -0300

    Alterando as primeiras propriedades

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+let texto = document.querySelector('p');
+
+titulo.innerHTML = 'Jogo do número secreto';
+texto.innerHTML = 'Escolha um número entre 1 e 10 e tente advinhar qual é o número secreto';
