aa12e969c3252e8b87b7569a881878f8b65cb92b
Mackenzzy <mackenzzy@gmail.com>
2023-10-18 23:15:26 -0300

    adicionado o comportamento de game over

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ function ExibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+exibirMensagemInicial();
 
-ExibirTextoNaTela('h1','Jogo do número secreto');
-ExibirTextoNaTela('p','Escolha um número entre 1 e 10 e tente advinhar qual é o número secreto');
+function exibirMensagemInicial(){
+    ExibirTextoNaTela('h1','Jogo do número secreto');
+    ExibirTextoNaTela('p','Escolha um número entre 1 e 10 e tente advinhar qual é o número secreto');
+    document.getElementById('chute').removeAttribute('disabled')
+}
+
+
 
 function verificarChute(){
     console.log ('chute');
@@ -25,8 +31,11 @@ function verificarChute(){
     if (chute == numeroSecreto){
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu com ${tentativas} ${palavraTentativa}`;
-        ExibirTextoNaTela('h1', 'Parabéns, você acertou')
-        ExibirTextoNaTela('p', mensagemTentativas)
+        ExibirTextoNaTela('h1', 'Parabéns, você acertou');
+        ExibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled')
+ 
+
     }else{
         ExibirTextoNaTela('h1', 'ERROU')
         if ( chute < numeroSecreto){
@@ -35,5 +44,28 @@ function verificarChute(){
             ExibirTextoNaTela('p', 'o numero secreto é menor')
         }
     tentativas++;
+    if(tentativas > 4){
+        
+        ExibirTextoNaTela('h1', 'GAME OVER');
+        ExibirTextoNaTela('p', 'Você esgotou o numero de tentativas');
+        document.getElementById('reiniciar').removeAttribute('disabled')
+        document.getElementById('chute').setAttribute('disabled', true)
+        //reinicar()
     }
+    limparCampo();
+    }
+
+
+}
+
+function     limparCampo(){
+    chute = document.querySelector('input')
+    chute.value = '';
+}
+function reinicar(){
+    numeroSecreto = gerarNumeroSecreto();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    
 }
