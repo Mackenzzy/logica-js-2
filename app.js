ff999ec1c7ad1b1a347ac4b002bea94fe9c4de40
Mackenzzy <mackenzzy@gmail.com>
2023-10-18 21:51:55 -0300

    Criando funcao

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ let texto = document.querySelector('p');
 
 titulo.innerHTML = 'Jogo do número secreto';
 texto.innerHTML = 'Escolha um número entre 1 e 10 e tente advinhar qual é o número secreto';
+
+function verificarChute(){
+    console.log ('chute')
+}
