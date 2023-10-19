7337bdd8f04f04b4fa0341a5daf40bf837ded243
Mackenzzy <mackenzzy@gmail.com>
2023-10-18 22:28:36 -0300

    funcao funcionando

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+let numeroSecreto = gerarNumeroSecreto();
+console.log (numeroSecreto)
+
 function ExibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-ExibirTextoNaTela('h1','Jogo do número secreto')
-ExibirTextoNaTela('p','Escolha um número entre 1 e 10 e tente advinhar qual é o número secreto')
+ExibirTextoNaTela('h1','Jogo do número secreto');
+ExibirTextoNaTela('p','Escolha um número entre 1 e 10 e tente advinhar qual é o número secreto');
 
 function verificarChute(){
-    console.log ('chute')
+    console.log ('chute');
+}
+
+function gerarNumeroSecreto(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto){
+        console.log ('parabens')
+    }else{
+        console.log ('errou otario')
+    }
 }
